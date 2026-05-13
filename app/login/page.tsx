7e7f456fc8ca748d55aae2274020d5c639305f51
thestiiiig <111060763+thestiiiig@users.noreply.github.com>
2026-05-13 22:10:29 +0000

    fix url

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -21,6 +21,7 @@ export default function LoginPage() {
       setError(error.message)
       setLoading(false)
     } else {
+      console.log("Attempted loginnnnnnnn")
       router.push('/dashboard')
     }
   }
